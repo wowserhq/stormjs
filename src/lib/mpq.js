725194170d71d73c28dad32d7340d0dfd39ead4c
fallenoak <git@fallenoak.me>
2018-01-08 22:47:14 -0600

    Add priority and flags to SFileOpenArchive call

--- src/lib/mpq.js
+++ src/lib/mpq.js
@@ -6,12 +6,13 @@ class MPQ {
   }
 }
 
-MPQ.open = async function (path) {
+MPQ.open = async function (path, flags = 0) {
   await StormLib.ready;
 
   const handle = new StormLib.Ptr();
+  const priority = 0;
 
-  if (StormLib.SFileOpenArchive(path, 0, 0, handle)) {
+  if (StormLib.SFileOpenArchive(path, priority, flags, handle)) {
     const mpq = new MPQ(handle);
   } else {
     throw 'Could not open MPQ!';
