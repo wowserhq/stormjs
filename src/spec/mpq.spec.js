9868decfddf7da14c4350174faa1f9fb7245276d
fallenoak <git@fallenoak.me>
2018-01-27 12:07:59 -0600

    Improve MPQ test suite

--- src/spec/mpq.spec.js
+++ src/spec/mpq.spec.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import { FS, MPQ } from '../lib';
+import StormLib from '../lib/stormlib';
 
 const rootDir = path.resolve(__filename, '../../../');
 
@@ -9,11 +10,52 @@ describe('MPQ', () => {
     FS.mount(FS.filesystems.NODEFS, { root: `${rootDir}/fixture` }, '/fixture');
   });
 
-  test('permits opening and closing an MPQ', async () => {
-    const mpq = await MPQ.open('/fixture/vanilla-standard.mpq');
+  describe('Opening / Closing', () => {
+    test('opens and closes an MPQ', async () => {
+      const mpq = await MPQ.open('/fixture/vanilla-standard.mpq');
 
-    mpq.close();
+      mpq.close();
 
-    expect(mpq).toBeInstanceOf(MPQ);
+      expect(mpq).toBeInstanceOf(MPQ);
+    });
+
+    test('throws if opening a nonexistent MPQ', async () => {
+      expect.assertions(1);
+
+      try {
+        const mpq = await MPQ.open('/fixture/nonexistent.mpq');
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+      }
+    });
+
+    test('throws if closing an MPQ with an invalid handle', async () => {
+      expect.assertions(1);
+
+      const mpq = await MPQ.open('/fixture/vanilla-standard.mpq');
+      const originalHandle = mpq.handle;
+      const invalidHandle = new StormLib.Ptr();
+
+      try {
+        mpq.handle = invalidHandle;
+        mpq.close();
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+      } finally {
+        mpq.handle = originalHandle;
+        mpq.close();
+      }
+    });
+
+    test('closes MPQ with noop if already closed', async () => {
+      const mpq = await MPQ.open('/fixture/vanilla-standard.mpq');
+
+      let result;
+
+      result = mpq.close();
+      result = mpq.close();
+
+      expect(result).toBeUndefined();
+    });
   });
 });
