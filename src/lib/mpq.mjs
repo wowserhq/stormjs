a6064d0a1fccaad58f7537293e97d06811fbcbb0
fallenoak <git@fallenoak.me>
2018-02-13 18:05:10 -0600

    Sort MPQ functions

--- src/lib/mpq.mjs
+++ src/lib/mpq.mjs
@@ -34,6 +34,21 @@ class MPQ {
     }
   }
 
+  openFile(fileName) {
+    if (this.handle) {
+      const fileHandle = new StormLib.VoidPtr();
+
+      if (StormLib.SFileOpenFileEx(this.handle, fileName, 0, fileHandle)) {
+        return new File(fileHandle);
+      } else {
+        fileHandle.delete();
+
+        const errno = StormLib.GetLastError();
+        throw new Error(`File could not be opened (error ${errno})`);
+      }
+    }
+  }
+
   search(mask, listfile = '') {
     this._ensureHandle();
 
@@ -68,21 +83,6 @@ class MPQ {
     findHandle.delete();
 
     return results;
-  }
-
-  openFile(fileName) {
-    if (this.handle) {
-      const fileHandle = new StormLib.VoidPtr();
-
-      if (StormLib.SFileOpenFileEx(this.handle, fileName, 0, fileHandle)) {
-        return new File(fileHandle);
-      } else {
-        fileHandle.delete();
-
-        const errno = StormLib.GetLastError();
-        throw new Error(`File could not be opened (error ${errno})`);
-      }
-    }
   }
 
   _ensureHandle() {
